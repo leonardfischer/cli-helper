14d46ebf79ac2fb8087b3f6fab13127ea9885ac7
Leonard Fischer <lfischer@i-doit.com>
2020-01-23 11:39:09 +0100

    Adding convenience classes BG and FG

---
+++ src/Color/BG.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace lfischer\cli\Color;
+
+use lfischer\cli\BackgroundColor;
+
+/**
+ * Class BG
+ * @package lfischer\cli\Color
+ */
+class BG extends BackgroundColor
+{
+
+}
---
+++ src/Color/FG.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace lfischer\cli\Color;
+
+use lfischer\cli\ForegroundColor;
+
+/**
+ * Class FG
+ * @package lfischer\cli\Color
+ */
+class FG extends ForegroundColor
+{
+
+}
