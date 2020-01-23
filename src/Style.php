21e2beec0227fd541a96a8ae200619a3a9d2efda
Leonard Fischer <lfischer@i-doit.com>
2020-01-23 12:00:41 +0100

    Adding style constant `RESET` as alias of `NONE`

--- src/Style.php
+++ src/Style.php
@@ -9,6 +9,7 @@ namespace lfischer\cli;
 class Style
 {
     public const NONE = "\033[0m";
+    public const RESET = "\033[0m";
     public const BOLD = "\033[1m";
     public const UNDERLINE = "\033[4m";
     public const NOUNDERLINE = "\033[24m";
