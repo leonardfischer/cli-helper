7963010573bd6c7b89d8741f100be40d53d36cbd
dev 0815 <post@leonardfischer.de>
2020-07-21 19:52:24 +0200

    Add `__callStatic` method for more convenience regarding CLI Styles

--- src/Style.php
+++ src/Style.php
@@ -4,7 +4,16 @@ namespace lfischer\cli;
 
 /**
  * Class Style
+ *
  * @package lfischer\cli
+ *
+ * @method static string none(string $message)
+ * @method static string reset(string $message)
+ * @method static string bold(string $message)
+ * @method static string underline(string $message)
+ * @method static string nounderline(string $message)
+ * @method static string negative(string $message)
+ * @method static string positive(string $message)
  */
 class Style
 {
@@ -15,4 +24,18 @@ class Style
     public const NOUNDERLINE = "\033[24m";
     public const NEGATIVE = "\033[7m";
     public const POSITIVE = "\033[27m";
+
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return string
+     */
+    public static function __callStatic(string $name, array $arguments): string
+    {
+        if (defined('self::' . strtoupper($name))) {
+            return constant('self::' . strtoupper($name)) . $arguments[0] . self::RESET;
+        }
+
+        return $arguments[0];
+    }
 }
